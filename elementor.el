f7567d3c1990e31d3d2d9b3adb94020e5e0cf77f
Mark Tomczak <iam@fixermark.com>
2015-05-15 17:51:08 -0400

    author --> code written by

--- elementor.el
+++ elementor.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (c) Google Inc. All Rights Reserved
 ;;
-;; author: Mark T. Tomczak (mtomczak@google.com)
+;; code written by: Mark T. Tomczak (mtomczak@google.com)
 
 ;; Permission is hereby granted, free of charge, to any person obtaining a copy
 ;; of this software and associated documentation files (the "Software"), to deal
